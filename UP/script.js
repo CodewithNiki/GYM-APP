3f285cf0b61035a7118a0364391a7864e10d10bf
CodewithNiki <101751820+CodewithNiki@users.noreply.github.com>
2022-11-25 20:22:02 +0100

    Section9/10/11

--- UP/script.js
+++ UP/script.js
@@ -1,3 +1,4 @@
+// header
 const header = document.querySelector(".header");
 const menu = document.querySelector(".header__menu");
 const overlay = document.querySelector(".header__overlay");
@@ -28,3 +29,18 @@ let smoothTrans = () => {
     htmlElement.classList.remove("transition");
   }, 1000);
 };
+
+// Testimonial
+const indicator = document.querySelectorAll(".img");
+const slider = document.querySelectorAll(".testimonials__content--text");
+
+for(let i=0; i<slider.length; i++){
+  indicator[i].addEventListener("click",()=>{
+    for(let j=0; j<slider.length; j++){
+      slider[j].classList.remove("show");
+      indicator[j].classList.remove("indicator");
+    }
+   slider[i].classList.add("show");
+   indicator[i].classList.add("indicator");
+  })
+}
